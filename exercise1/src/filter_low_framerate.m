2ee756304418840c78e8b852953c79a96dd2b01a
wl <wl@viennalinux.at>
2011-11-04 17:11:19 +0000

    filter_low_framerate.m is now COMPLETE (source+documentation)

--- exercise1/src/filter_low_framerate.m
+++ exercise1/src/filter_low_framerate.m
@@ -14,10 +14,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   IMPLEMENTATION:
-%       .....
+%       We first create an array called double_frame that is of size
+%       source_fps. This array is made persistent in memory for this
+%       function so every successive call can read it. The array is
+%       initializer with zeros meaning that no frame has to be doubled. A
+%       value of 1 means that the current image is discarded and copied
+%       from the previous one.
+%       Only if source and target fps do not match we calculate what frames
+%       have to be doubled to reach source_fps but with only target_fps
+%       unique images. With our algorithm frames that need not to be
+%       doubled get equidistantly aligned.
+%       Every frame then gets looked up by it's frame number modulo the
+%       source_fps in the double_frame array. It then gets either doubled
+%       from the previous frame (lookup value is 1) or not (lookup value is
+%       0)
 %
 %   PHYSICAL BACKGROUND:
-%       .....
+%       Early movie recording devices were no capable of recording at high
+%       frame rates. Early silent movies used framerates ranging from 7 to
+%       20 fps while todays hollywood blockbusters are (still) recorded at
+%       24 fps. Playback often happens at higher framerates and therefore
+%       it's necessary to double frames to reach for example 25/50/60/100/200
+%       Hz.
 function video = filter_low_framerate(video, source_fps, target_fps)
 
     % persistant array in memory for this function
