5579d19ade3c22a5d02eb4f6931d27cbd3900760
wl <wl@viennalinux.at>
2011-11-04 17:38:08 +0000

    implementation details and physical backgrounds added

--- exercise1/src/filter_unsharp.m
+++ exercise1/src/filter_unsharp.m
@@ -13,13 +13,24 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   IMPLEMENTATION:
-%       .....
+%       We create a gaussian filter kernel (that we use for convolution)
+%       with the command fspecial. The filterSize determines the kernels
+%       size whereas sigma is the standard deviation. The current frame is
+%       then convoluted with the created filterKernel and written-back to
+%       the current frame data.
 %
 %   PHYSICAL BACKGROUND:
-%       .....
+%       If the image plane is not in focus of the optical system things get
+%       blurred. The circle of confusion (CoC) is then larger than one
+%       pixel. Normally this depends on the distance of an object but we
+%       blur the whole image as an approximation. In the early days optical
+%       systems were not that accuarate and things like autofocus did not
+%       exist so a lot of things were out of focus and blurry.
 %
 %   RANGE VALUES FOR PARAMETERS:
-%       .....
+%       filterSize is the dimension of the filterKernel square matrix and
+%       should be odd > 1 to have a nice filterkernel (3, 5, 7 and so on).
+%       sigma is the standard deviation an should be a positive value.
 function video = filter_unsharp(video, filterSize, sigma)
     filterkernel = fspecial('gaussian', filterSize, sigma);
     video.frame(1).filtered = imfilter(video.frame(1).filtered, filterkernel,'replicate', 'same', 'conv');
