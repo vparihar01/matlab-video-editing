70dc05d543ac297631fcfdf9ac7136ccd8fa0709
stefan.trumpf <stefan.trumpf@gmail.com>
2011-11-05 12:28:13 +0000

    high contrast is not working correctly yet

--- exercise1/src/filter_highcontrast.m
+++ exercise1/src/filter_highcontrast.m
@@ -30,9 +30,22 @@ function video = filter_highcontrast(video, dx, dy)
     % map intensity(V=VALUE) values to [0..255]
     img(:,:,3) = img(:,:,3) * 255;
     % map each intensity value with the stepfunction
+    k1 = (dy/dx)*255;
+    k2 = ((1-2*dy)/(1-2*dx))*255;
+    intensities = zeros(size(img,1),size(img,2),3);
+    intensities(:,:,1) = (img(:,:,3) <= (dx*255)) .* img(:,:,3);
+    intensities(:,:,2) = ((img(:,:,3) < ((1-dx)*255)) .* img(:,:,3) ) - intensities(:,:,1);
+    intensities(:,:,3) = img(:,:,3) - intensities(:,:,2);
     
+    intensities(:,:,1) = intensities(:,:,1) * k1;
+    intensities(:,:,2) = (intensities(:,:,2) * k2) + (dy*255);
+    intensities(:,:,3) = (intensities(:,:,3) * k1) + ((1-dy)*255);
+    img(:,:,3) = intensities(:,:,1) + intensities(:,:,2) + intensities(:,:,3);
     % convert intensity(HS,V=VALUE) values back again to [0..1]
     img(:,:,3) = img(:,:,3) / 255;
+    temp = img(:,:,3);
+    temp(temp > 1) = 1;
+    img(:,:,3) = temp;
     % convert the image back to RGB colorspace
     video.frame(1).filtered = hsv2rgb(img);
 end
