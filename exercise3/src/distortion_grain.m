51699f79af2ed04eb99b3ce021a640c1b1c67f1b
wl <wl@viennalinux.at>
2011-12-12 18:27:54 +0000

    distortion_grain Dokumentation noch aufgepeppt

--- exercise3/src/distortion_grain.m
+++ exercise3/src/distortion_grain.m
@@ -16,16 +16,26 @@
 %       Varianz um den Mittelwert 0.
 %       Der Parameter "variance" stellt die Varianz dar, wobei man keinen
 %       Mittelwert übergeben muss, da dieser immer als 0 genommen wird.
+%       Ein variance-Wert von 0.005 erzeugt ein glaubwürdiges Ausgabebild.
 %       Das Rauschen wird auf jeden der 3 Farbkanäle angewandt und erzeugt
 %       dadurch ein buntes Rauschen im Ausgabebild.
 %   
 %   PHYSICAL BACKGROUND:
+%     How is it caused?
 %       "Filmkorn nennt man in der Fotografie die kleinsten Strukturen des
 %       entwickelten Films, die erst bei starker Vergrößerung gut sichtbar
 %       werden. Fotochemisch handelt es sich dabei um Zusammenballungen von
 %       feinst verteilten Silberpartikeln, die für die Zerrissenheit
 %       gleichmäßig geschwärzter Flächen verantwortlich sind.
-%       Die Fein- oder Grobkörnigkeit ist sehr vom Filmtyp, aber auch von
+%
+%     Which movies are effected by these distortions?
+%       Alle Filme sind davon betroffen, da allen analogen Filmen ein
+%       photochemisches Material zu Grunde liegt. Allen digitalen Filmen
+%       liegt ein Bildsensor zu Grunde welche ebenfalls eine
+%       Rausch-charakteristik aufweist.
+%
+%     Can the distortion be avoided? If so, how?
+%       "Die Fein- oder Grobkörnigkeit ist sehr vom Filmtyp, aber auch von
 %       den Entwicklungsbedingungen abhängig. Filme mit niedriger
 %       Lichtempfindlichkeit, also mit niedrigen ISO-Werten, sind in der
 %       Regel sehr viel feinkörniger als stark lichtempfindliche Filme mit
@@ -34,6 +44,10 @@
 %       durchaus weniger störendes Korn aufweisen als mittelempfindliche
 %       Filme (ISO 50 bis ISO 100) aus den 1970er Jahren."
 %       Quelle: http://de.wikipedia.org/wiki/Filmkorn
+%       In der heutigen digitalfotografie findet "nur" noch ein Rauschen
+%       des digitalen Bildsensors statt, welches aber stetig besser
+%       (geringer) ausfällt und außerdem durch digitale
+%       Rauschunterdrückungsalgorithmen noch verbessert werden kann.
 %
 function video = distortion_grain(video, variance)
 
